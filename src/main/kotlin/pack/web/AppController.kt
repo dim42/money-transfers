0d93725b8c5db5e07be0829f6c977b495c378253
dim42 <dimt777@gmail.com>
2020-03-10 04:59:15 +0300

    Wait for listening - to prevent ConnectException

--- src/main/kotlin/pack/web/AppController.kt
+++ src/main/kotlin/pack/web/AppController.kt
@@ -2,17 +2,19 @@ package pack.web
 
 import io.netty.handler.codec.http.HttpHeaderValues.APPLICATION_JSON
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.Promise
 import io.vertx.core.Vertx
 import io.vertx.core.VertxOptions
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.handler.BodyHandler
+import org.slf4j.LoggerFactory
 import pack.dao.Repository
 import pack.dao.RepositoryImpl
 import pack.service.AccountService
 import pack.service.PaymentService
 
 class AppController : AbstractVerticle() {
-    override fun start() {
+    override fun start(startPromise: Promise<Void>?) {
         val repository: Repository = RepositoryImpl(vertx.sharedData())
         val accountService = AccountService(repository)
         val paymentService = PaymentService(repository)
@@ -31,10 +33,19 @@ class AppController : AbstractVerticle() {
 
         val mainRouter = Router.router(vertx)
         mainRouter.mountSubRouter(ROOT_PATH + API_VERSION, restAPI)
-        vertx.createHttpServer().requestHandler(mainRouter).listen(PORT, HOST)
+        vertx.createHttpServer().requestHandler(mainRouter).listen(PORT, HOST) { result ->
+            if (result.succeeded()) {
+                startPromise?.complete()
+            } else {
+                log.error("Start error: {}", result)
+                startPromise?.fail(result.cause())
+            }
+        }
     }
 
     companion object {
+        private val log = LoggerFactory.getLogger(AppController::class.java)
+
         private const val HOST = "localhost"
         private const val PORT = 8081
         private const val ROOT_PATH = "/api/"
